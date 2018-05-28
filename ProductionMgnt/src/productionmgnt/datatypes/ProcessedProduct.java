d56185d8a209efd10040fe8c9d74fd8bf8b474fb
Martin Wudenka <Martin.Wudenka@gmx.de>
2018-05-28 13:18:47 +0200

    Implement getStatus on ProductionMgnt

--- ProductionMgnt/src/productionmgnt/datatypes/ProcessedProduct.java
+++ ProductionMgnt/src/productionmgnt/datatypes/ProcessedProduct.java
@@ -4,8 +4,15 @@ import commontypes.datatypes.Product;
 
 public class ProcessedProduct {
 	
+	private String id;
 	private String status;
 	private Product product;
+	
+	public ProcessedProduct(String id, Product product) {
+		this.setId(id);
+		this.product = product;
+		this.status = "waiting";
+	}
 
 	public String getStatus() {
 		return status;
@@ -21,6 +28,14 @@ public class ProcessedProduct {
 
 	public void setProduct(Product product) {
 		this.product = product;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 }
--- ProductionMgnt/src/productionmgnt/services/ProductionMgnt.java
+++ ProductionMgnt/src/productionmgnt/services/ProductionMgnt.java
@@ -8,22 +8,29 @@ import commontypes.services.IProductionMgnt;
 import productionmgnt.datatypes.ProcessedProduct;
 
 public class ProductionMgnt implements IProductionMgnt {
-	
+
 	private List<ProcessedProduct> processedProducts;
-	
+	private int idCounter = 0;
+
 	public ProductionMgnt() {
 		processedProducts = new LinkedList<ProcessedProduct>();
 	}
 
 	@Override
 	public String comissionProduct(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		ProcessedProduct p = new ProcessedProduct("product" + idCounter++, product);
+		processedProducts.add(p);
+
+		return p.getId();
 	}
 
 	@Override
 	public String getStatus(String id) {
-		// TODO Auto-generated method stub
+		for (ProcessedProduct p : this.processedProducts) {
+			if (id.equals(p.getId())) {
+				return p.getStatus();
+			}
+		}
 		return null;
 	}
 
